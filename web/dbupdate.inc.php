3b7b4fded30ce5dc25f59842fa7ae02aa41e8499
Tyler <j.ty.hall@gmail.com>
2017-02-23 17:57:52 -0600

    updates

--- web/dbupdate.inc.php
+++ web/dbupdate.inc.php
@@ -113,7 +113,7 @@ if($nogo_point==0){
     NO MINVALUE
     NO MAXVALUE
     CACHE 1");
-	logDoQuery($dbu,"ALTER TABLE ONLY nogo_zone ALTER COLUMN id SET DEFAULT nextval('nogo_zone_id_seq'::regclass)");
+	logDoQuery($dbu,"ALTER TABLE ONLY nogo_point ALTER COLUMN id SET DEFAULT nextval('nogo_zone_id_seq'::regclass)");
 	logDoQuery($dbu,"ALTER TABLE ONLY nogo_zone
     ADD CONSTRAINT nogo_zone_pkey PRIMARY KEY (id)");
 }
@@ -126,14 +126,14 @@ if($nogo_zone==0){
     maxtvd double precision,
     show_plot smallint DEFAULT 0 NOT NULL,
     show_report smallint DEFAULT 0 NOT NULL)");
-	logDoQuery($dbu,"CREATE SEQUENCE nogo_zone_id_seq
+	logDoQuery($dbu,"CREATE SEQUENCE nogo_point_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1");
-	logDoQuery($dbu,"ALTER TABLE ONLY nogo_zone ALTER COLUMN id SET DEFAULT nextval('nogo_zone_id_seq'::regclass)");
-	logDoQuery($dbu,"ALTER TABLE ONLY nogo_zone
+	logDoQuery($dbu,"ALTER TABLE ONLY nogo_point ALTER COLUMN id SET DEFAULT nextval('nogo_point_id_seq'::regclass)");
+	logDoQuery($dbu,"ALTER TABLE ONLY nogo_point
     ADD CONSTRAINT nogo_zone_pkey PRIMARY KEY (id)");
 }
 // check if the rotate/slide table is present
