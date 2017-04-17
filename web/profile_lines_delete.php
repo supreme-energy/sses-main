d9ca7c9cd9e7c7db65d4f8eaa72196d8da3488a9
Tyler Hall <j.ty.hall@gmail.com>
2017-04-17 11:26:59 -0500

    updates

--- web/profile_lines_delete.php
+++ web/profile_lines_delete.php
@@ -9,7 +9,7 @@ $db->OpenDb();
 $sql = "delete from profile_lines where reference_database='$dbname'";
 $db->DoQuery($sql);
 $result = json_encode("done");
-$request_back_to = str_replace("_add","",$_SERVER[REQUEST_URI]);
+$request_back_to = str_replace("_delete","",$_SERVER[REQUEST_URI]);
 $actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$request_back_to";
 header("Location: $actual_link"); /* Redirect browser */
 exit();
