01d0d288c64c9c891cb45a643ad8d4520154d6a4
Tyler Hall <j.ty.hall@gmail.com>
2017-05-24 17:28:05 -0500

    updates

--- web/test/getlogdata_test.php
+++ web/test/getlogdata_test.php
@@ -1,4 +1,6 @@
 <?php 
+require_once("../dbio.class.php");
+require_once("../classes/WitsmlData.class.php");
 $seldbname=$_REQUEST['seldbname'];
 require_once('../classes/PolarisConnection.class.php');
 header('Content-type: application/json');
