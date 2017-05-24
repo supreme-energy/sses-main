e2e3d523ed35f18fd9c4f533f857a74fea63cd9f
Tyler Hall <j.ty.hall@gmail.com>
2017-05-24 17:26:18 -0500

    updates

---
+++ web/test/getlogdata_test.php
@@ -0,0 +1,8 @@
+<?php 
+$seldbname=$_REQUEST['seldbname'];
+require_once('../classes/PolarisConnection.class.php');
+header('Content-type: application/json');
+$obj= new PolarisConnection($_REQUEST);
+$obj->autorc_type=$autorc_type;
+$obj->$this->prepare_las_data(0,8000,1,false,false,false);
+?>
