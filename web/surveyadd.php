1cef429e114b3de8f7dec5a56125cb8943b96e2a
Tyler Hall <j.ty.hall@gmail.com>
2017-05-30 14:32:07 -0500

    $witsml->prepare_las_data(8000,8300,1);

--- web/surveyadd.php
+++ web/surveyadd.php
@@ -12,11 +12,25 @@ $inc=$_POST['inc']; if($inc=="") $inc=0;
 $azm=$_POST['azm']; if($azm=="") $azm=0;
 $ret=(isset($_POST['ret']) ? $ret = $_POST['ret'] : 'gva_tab3.php');
 require_once("dbio.class.php");
+require_once("classes/WitsmlData.class.php");
 $db=new dbio($seldbname);
 $db->OpenDb();
+include("readwellinfo.inc.php");
+$db->DoQuery("select  * from surveys where plan=0 order by md desc limit 1");
+$frow = $db->FetchRow();
 $db->DoQuery("INSERT INTO surveys (md,inc,azm) VALUES ($md,$inc,$azm)");
 $db->DoQuery("UPDATE wellinfo SET pamethod='-1';");
 $db->DoQuery("delete from projections where ptype='rot' or ptype='sld'");
+
+if($autorc_type=='polaris'){
+	if($frow['md']>0 && $frow['md']<$md ){
+		$smd=$frow['md'];
+	} else{
+		$smd=0;
+	}
+	$emd=$md;
+	$witsml->prepare_las_data($smd,$emd);
+}
 exec("./sses_gva -d $seldbname");
 exec("./sses_cc -d $seldbname");
 exec("./sses_cc -d $seldbname -p");
