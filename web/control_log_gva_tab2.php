0e8016c8e55352946f4dc4c8333ffcb04a816ac6
Tyler <tyler.hall@acimacredit.com>
2017-12-06 15:31:55 -0600

    updated to use reversed range instead of negative md

--- web/control_log_gva_tab2.php
+++ web/control_log_gva_tab2.php
@@ -4,8 +4,16 @@
 	$y_plot = Array();
     $db->DoQuery("select * from $tablename where md > $startmd and md <= $endmd order by md");
     $max_y = 0;
+    $min_x = 0;
+    $max_x = 0;
 	while($db->FetchRow()){
-		array_push($x_plot, $db->FetchField("md")*-1);
+		array_push($x_plot, $db->FetchField("md"));
+		if($db->FetchField("md") > $max_x){
+			$max_x = $db->FetchField("md");
+		}
+		if($min_x == 0 || $db->FetchField("md") < $min_x ){
+			$min_x = $db->FetchField("md");
+		}
 		array_push($y_plot, $db->FetchField("value"));
 		if($db->FetchField("value") > $max_y){
 			$max_y = $db->FetchField("value");
@@ -13,10 +21,28 @@
 	}
 	$db->DoQuery("select * FROM controllogs LIMIT 1;");
 	$db->FetchRow();
-	$tcl_x   = $db->FetchField("tot")*-1;
-	$tcl_y   = $db->FetchField("bot")*-1;
+	$tcl_x   = $db->FetchField("tot");
+	$tcl_y   = $db->FetchField("bot");
 ?>
 <script>
+var layout = {
+		  height: 950,
+		  margin: {
+			l: 25,
+			r: 25,
+			t: 25,
+			b: 25
+		  },
+		  xaxis: {
+			autorange: false,
+			range: [0,<?php echo $max_y+30?>]
+		  },
+		  yaxis: {
+          autorange: 'reversed',
+          range: [<?php echo $min_x?>, <?php echo $max_x ?>],
+	      }
+		};
+		
 var trace2 = {
 		y: [<?php echo $tcl_x?>,<?php echo $tcl_x?>],
 		x: [<?php echo $tcl_y?>,<?php echo $max_y * 2?> ],
@@ -32,19 +58,7 @@ var trace1 = {
 
 
 
-var layout = {
-		  height: 950,
-		  margin: {
-			l: 25,
-			r: 25,
-			t: 25,
-			b: 25
-		  },
-		  xaxis: {
-			autorange: false,
-			range: [0,<?php echo $max_y+30?>]
-		  }
-		};
+
 var data = [trace1,trace2];
 </script>
 <div id='control_log_plot' '></div>
