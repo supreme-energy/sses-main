cc1b2459b6d45bfaa8885351cd823ef3f8d06f86
Tyler <tyler.hall@acimacredit.com>
2018-05-21 12:27:48 -0600

    added data truncation to allow large file processing

--- web/upload_raw_file.php
+++ web/upload_raw_file.php
@@ -1,4 +1,11 @@
 <?php 
 	$fileContent = file_get_contents($_FILES['userfile']['tmp_name']);
-	echo $fileContent; 
+	$lines = explode("\n", $fileContent); 
+    $pre_truncation_lines = count($lines);
+	$lines = array_slice($lines, 0, 500); //10 is how many lines you want to keep
+	$text = implode("\n", $lines);
+	if($pre_truncation_lines > 500){
+		$text .="\n\n-------DATA-TRUNCATED-FOR-DISPLAY---------------";
+	}
+	echo $text; 
 ?>
