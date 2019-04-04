c29698b248e3b6958064d1e3492966e6c6c96525
Tyler <tyler.hall@acimacredit.com>
2019-04-04 14:10:58 -0600

    update joblist

--- web/api/joblist.php
+++ web/api/joblist.php
@@ -1,16 +1,14 @@
 <?php 
 require_once("../dbio.class.php");
-if(!isset($seldbname) or $seldbname == '')	$seldbname = (isset($_GET['seldbname']) ? $_GET['seldbname'] : ''
-$dbids=array();
-$dbnames=array();
-$realnames=array();
+if(!isset($seldbname) or $seldbname == '')	$seldbname = (isset($_GET['seldbname']) ? $_GET['seldbname'] : '';
+
+
 $db=new dbio("sgta_index");
 $db->OpenDb();
 $db->DoQuery("SELECT * FROM dbindex ORDER BY id DESC;");
 $response = array();
 while($db->FetchRow()) {
 	$id=$db->FetchField("id");
-	$dbids=$id;
 	$dbn=$db->FetchField("dbname");
 	$dbreal=$db->FetchField("realname");
 	$response->push({"sgta_index_id" => $id, "jobname" => $dbn, "realjobname" => $dbreal})	
