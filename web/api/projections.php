6b76ac437868cbab730c2c053e0d9721552f9b6a
Tyler <tyler.hall@acimacredit.com>
2019-04-04 14:28:51 -0600

    added projections api file

---
+++ web/api/projections.php
@@ -0,0 +1,62 @@
+ <?php
+ include("api_header.php");
+ $db2=new dbio($seldbname);
+ $db2->OpenDb();
+ $db->DoQuery("select * from addforms");
+ $totid =null;
+ $botid = null;
+ while($db->FetchRow()){
+ 
+ 	if(trim($db->FetchField('label'))=='TOT'){
+ 		$totid = $db->FetchField('id');
+ 	}
+ 	if(trim($db->FetchField('label'))=='BOT'){
+ 		$botid = $db->FetchField('id');
+ 	}
+ }
+ $projections_joined = array();
+ $db->DoQuery("SELECT * FROM projections ORDER BY md $surveysort;");
+ while($db->FetchRow()){
+ 	if($totid){
+ 		if($plan){
+ 			$query = "select tot from addformsdata where md=$md and infoid=$totid;";
+ 		}else{
+ 			$query = "select tot from addformsdata where projid=$svyid and infoid=$totid;";
+ 		}
+ 		$db2->DoQuery($query);
+ 		$db2->FetchRow();
+ 		$tot =sprintf("%.2f", $db2->FetchField("tot"));
+ 	}
+ 	if($botid){
+ 		if($plan){
+ 			$query = "select tot from addformsdata where md=$md and infoid=$botid;";
+ 		}else{
+ 			$query = "select tot from addformsdata where projid=$svyid and infoid=$botid;";
+ 		}
+ 		$db2->DoQuery($query);
+ 		$db2->FetchRow();
+ 		$bot =sprintf("%.2f", $db2->FetchField("tot"));
+ 	}
+ 	$projections_joined[]= array(
+ 			'id' => $db->FetchField("id") ,
+ 			'md' => sprintf("%.2f",$db->FetchField("md")) ,
+ 			'inc' =>sprintf("%.2f", $db->FetchField("inc")),
+ 			'azm' =>sprintf("%.2f", $db->FetchField("azm")),
+ 			'tvd' =>$db->FetchField("tvd"),
+ 			'vs' =>sprintf("%.2f", $db->FetchField("vs")),
+ 			'ns' =>sprintf("%.2f", $db->FetchField("ns")),
+ 			'ew' =>sprintf("%.2f", $db->FetchField("ew")),
+ 			'ca' =>sprintf("%.2f", $db->FetchField("ca")),
+ 			'cd' =>sprintf("%.2f", $db->FetchField("cd")),
+ 			'dl' =>sprintf("%.2f", $db->FetchField("dl")),
+ 			'cl' =>sprintf("%.2f", $db->FetchField("cl")),
+ 			'tcl'=>$db->FetchField("tot"),
+ 			'tot'=>$tot,
+ 			'bot'=>$bot,
+ 			'dip'=>sprintf("%.2f", $db->FetchField("dip")),
+ 			'fault'=>sprintf("%.2f", $db->FetchField("fault")),
+ 			'method'=>$db->FetchField("method")
+ 	);
+ }
+ echo json_encode($projections_joined);
+ ?>
