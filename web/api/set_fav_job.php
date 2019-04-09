3400aaf2820516735289ce4f17d9da8bd2e4e96d
Tyler <tyler.hall@acimacredit.com>
2019-04-09 13:42:23 -0600

    escapes with mysql escape

--- web/api/set_fav_job.php
+++ web/api/set_fav_job.php
@@ -3,11 +3,11 @@
   header("Access-Control-Allow-Origin: *");
   header('Content-type: application/json');
   require_once '../dbio.class.php';
-  if(!isset($seldbname) or $seldbname == '') $seldbname = pg_escape_literal((isset($_REQUEST['seldbname']) ? $_REQUEST['seldbname'] : ''));
-  $fav_val = pg_escape_literal($_REQUEST['favorite']);
+  if(!isset($seldbname) or $seldbname == '') $seldbname = mysql_real_escape_string((isset($_REQUEST['seldbname']) ? $_REQUEST['seldbname'] : ''));
+  $fav_val = mysql_real_escape_string($_REQUEST['favorite']);
   $db=new dbio("sgta_index");
   $db->OpenDb();
-  $query = "update dbindex set favorite=$fav_val where dbname=$seldbname";
+  $query = "update dbindex set favorite='$fav_val' where dbname='$seldbname'";
   $db->DoQuery($query);
   $response = array("status"=>"success", "message" => "operation successful");
   echo json_encode($response);
