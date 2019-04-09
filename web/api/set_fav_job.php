e104b9f6b84d54ea89b44a145076ef8bc95e1d9d
Tyler <tyler.hall@acimacredit.com>
2019-04-09 13:43:45 -0600

    removed escape for now

--- web/api/set_fav_job.php
+++ web/api/set_fav_job.php
@@ -3,8 +3,8 @@
   header("Access-Control-Allow-Origin: *");
   header('Content-type: application/json');
   require_once '../dbio.class.php';
-  if(!isset($seldbname) or $seldbname == '') $seldbname = mysql_real_escape_string((isset($_REQUEST['seldbname']) ? $_REQUEST['seldbname'] : ''));
-  $fav_val = mysql_real_escape_string($_REQUEST['favorite']);
+  if(!isset($seldbname) or $seldbname == '') $seldbname = (isset($_REQUEST['seldbname']) ? $_REQUEST['seldbname'] : '');
+  $fav_val = $_REQUEST['favorite'];
   $db=new dbio("sgta_index");
   $db->OpenDb();
   $query = "update dbindex set favorite='$fav_val' where dbname='$seldbname'";
