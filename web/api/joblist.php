608e903363f0568437d318968882661db8682c54
Tyler <tyler.hall@acimacredit.com>
2019-04-09 09:59:21 -0600

    added long/lat

--- web/api/joblist.php
+++ web/api/joblist.php
@@ -9,7 +9,31 @@ while($db->FetchRow()) {
 	$id=$db->FetchField("id");
 	$dbn=$db->FetchField("dbname");
 	$dbreal=$db->FetchField("realname");
-	array_push($response, array("sgta_index_id" => $id, "jobname" => $dbn, "realjobname" => $dbreal));	
+	$db2 = new dbio($dbn);
+	$db2->OpenDb();
+	$db2->DoQuery("select * from wellinfo");
+	$pbhl_easting=$db2->FetchField("pbhl_easting");
+	$pbhl_northing=$db2->FetchField("pbhl_northing");
+	$survey_easting=$db2->FetchField("survey_easting");
+	$survey_northing=$db2->FetchField("survey_northing");
+	$landing_easting=$db2->FetchField("landing_easting");
+	$landing_northing=$db2->FetchField("landing_northing");
+	$elev_ground=$db2->FetchField("elev_ground");
+	$elev_rkb=$db2->FetchField("elev_rkb");
+	$correction=$db2->FetchField("correction");
+	$coordsys=$db2->FetchField("coordsys");
+	array_push($response, array("sgta_index_id" => $id, "jobname" => $dbn, "realjobname" => $dbreal,
+			"pbhl_easting" => $pbhl_easting,
+			"pbhl_northing" => $pbhl_northing,
+			"survey_easting" => $survey_easting,
+			"survey_northing" => $survey_northing,
+			"landing_easting" => $landing_northing,
+			"elev_groun" => $elev_ground,
+			"elev_rkb"   => $elev_rkb,
+			"correction" => $correction,
+			"coordsys"   => $coordsys
+	));	
+	$db2->CloseDb();
 }
 echo json_encode($response);	
 ?>
