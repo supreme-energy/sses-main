4a9439f058f18a39dafa3874b9f275d338426528
Tyler <tyler.hall@acimacredit.com>
2019-04-12 12:38:35 -0600

    added wellloglist api call

---
+++ web/api/welloglist.php
@@ -0,0 +1,33 @@
+ <?php
+	include("api_header.php");
+	$db->DoQuery("SELECT * FROM welllogs order by startmd asc");
+	$with_data = $_REQUEST['data'] == '1';
+	while($db->FetchRow()){
+		$id = $db->FetchField("id");
+		$tablename = $db->FetchField("tablename");
+		$result = array(
+				"id" => $id,
+				"tablename" =>$tablename,
+				"realname" => $db->FetchField('realname'),
+				"startmd"  => $db->FetchField('startmd'),
+				"endmd"    => $db->FetchField('endmd'),
+				"starttvd" => $db->FetchField('starttvd'),
+				"endtvd"   => $db->FetchField('endtvd'),
+				"startvs"  => $db->FetchField('startvs'),
+				"endvs"    => $db->FetchField('endvs'),
+				"startdepth" => $db->FetchField('startdepth'),
+				"enddepth"   => $db->FetchField('enddepth'),
+				"fault"      => $db->FetchField('fault'),
+				"sectdip"    => $db->FetchField('dip'),
+				"secttot"    => $db->FetchField('tot'),
+				"sectbot"    => $db->FetchField('bot'),
+				"scalebias"  => $db->FetchField('scalebias'),
+				"scalefactor" => $db->FetchField('scalefactor')				
+		);
+		if($with_data){
+			include("read_welllog_include.php");
+			$result['data'] = $data;
+		}
+		array_push($results, $result);
+	}
+ ?>
