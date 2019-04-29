00a4d87ccdf445301b976aa6acce2a71ee487ad8
Tyler <tyler.hall@acimacredit.com>
2019-04-29 13:53:55 -0600

    added execs

--- web/api/setsurveyfield.php
+++ web/api/setsurveyfield.php
@@ -13,5 +13,9 @@ if(count($updates_array) > 0 ){
 	$query = "update surveys set ". implode($updates_array, ',') . " where id=$id";
 	$db->DoQuery($query);
 }
+exec("./sses_gva -d $seldbname ");
+exec("./sses_cc -d $seldbname");
+exec("./sses_cc -d $seldbname -p");
+exec ("./sses_af -d $seldbname");
 echo json_encode(array("status" => "Success", "message" => "operation completed"));
 ?>
