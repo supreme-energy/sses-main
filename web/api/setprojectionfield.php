660262d693f9b999464a2d97d4d2bd572d08aa2c
Tyler <tyler.hall@acimacredit.com>
2019-04-29 13:38:50 -0600

    added setprojectionfield, setsurveyfield to api

---
+++ web/api/setprojectionfield.php
@@ -0,0 +1,14 @@
+<?
+include("api_header.php");
+$id = $_REQUEST['id'];
+$field_names = array('md', 'inc', 'azm', 'tvd', 'vs', 'ns', 'ew', 'ca', 'cd', 'dl', 'cl', 'bot', 'dip', 'fault', 'method');
+$updates_array = array();
+foreach($field_names as $field_name){
+	if(isset($_REQUEST[$field_name])){
+		$value = $_REQUEST[$field_name];
+		array_push($updates_array, "$field_name = '$value'");	
+	}
+}
+$query = "update projections set ". implode($updates_array, ',') . ' where id=$id'
+echo $query;
+?>
