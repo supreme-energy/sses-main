d5537c2dcefac5082a79f66f7636f4cdd42c68b6
Tyler <tyler.hall@acimacredit.com>
2019-05-16 09:28:33 -0600

    updated survey updatier to support new fields

--- web/api/setsurveyfield.php
+++ web/api/setsurveyfield.php
@@ -1,11 +1,15 @@
 <?
 include("api_header.php");
 $id = $_REQUEST['id'];
-$field_names = array('md', 'inc', 'azm', 'tvd', 'vs', 'ns', 'ew', 'ca', 'cd', 'dl', 'cl', 'bot', 'dip', 'fault');
+$field_names = array('md', 'inc', 'azm', 'tvd', 'vs', 'ns', 'ew', 'ca', 'cd', 'dl', 'cl', 'bot', 'dip', 'fault', 'created_at', 'unixtime_src');
 $updates_array = array();
 foreach($field_names as $field_name){
 	if(isset($_REQUEST[$field_name])){
 		$value = $_REQUEST[$field_name];
+		$query_field_name = $field_name;
+		if($field_name == 'unixtime_src'){
+			$query_field_name = 'srcts';
+		}
 		array_push($updates_array, "$field_name = '$value'");	
 	}
 }
