e298dae4373a634beb88fab964243b3b81887119
Tyler <tyler.hall@acimacredit.com>
2019-06-17 13:12:26 -0600

    updated paths

--- web/api/setprojectionfield.php
+++ web/api/setprojectionfield.php
@@ -110,9 +110,9 @@ if($autoposdec>0){
     }
     $db2->CloseDb();
 }
-exec("./sses_gva -d $seldbname ");
-exec("./sses_cc -d $seldbname");
-exec("./sses_cc -d $seldbname -p");
-exec ("./sses_af -d $seldbname");
+exec("../sses_gva -d $seldbname ");
+exec("../sses_cc -d $seldbname");
+exec("../sses_cc -d $seldbname -p");
+exec ("../sses_af -d $seldbname");
 echo json_encode(array("status" => "Success", "message" => "operation completed"));
 ?>
