f93c5ee7d5531ddd575ad11748c73ecc41cc0e5a
Tyler <tyler.hall@acimacredit.com>
2019-06-25 12:17:03 -0600

    updated

--- web/api/lastupdated.php
+++ web/api/lastupdated.php
@@ -1,3 +1,7 @@
 <?php 
-    $tables_to_check = ['surveys','projections','formations', 'wellinfo', 'appinfo'];
+include("api_header.php");
+$query ="select greatest(max(t1.updated_at),max(t2.updated_at), max(t3.updated_at), max(t4.updated_at)) as last_updated from surveys as t1, projections as t2, appinfo as t3, wellinfo as t4;";
+$db->DoQuery($query);
+$db->FetchRow();
+echo json_encode(array('last_updated'=> $db->FetchField('last_updated')));
 ?>
