3a071892a2f189e557fb4c50b77e003354d08af7
Tyler <tyler.hall@acimacredit.com>
2019-07-18 17:33:27 -0600

    updated pathing

--- web/api/api_header.php
+++ web/api/api_header.php
@@ -2,7 +2,7 @@
   error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
   header("Access-Control-Allow-Origin: *");
   header('Content-type: application/json');
-  require_once '../dbio.class.php';
+  require_once(_DIR__.'../dbio.class.php');
   if(!isset($seldbname) or $seldbname == '') $seldbname = (isset($_REQUEST['seldbname']) ? $_REQUEST['seldbname'] : '');
   $db=new dbio($seldbname);
   $db->OpenDb();
