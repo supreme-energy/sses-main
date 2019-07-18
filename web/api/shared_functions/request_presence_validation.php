c86d5ed76fe0d2290c539e6fe5bcebc244f01cf1
Tyler <tyler.hall@acimacredit.com>
2019-07-18 17:49:33 -0600

    fixed error checking

--- web/api/shared_functions/request_presence_validation.php
+++ web/api/shared_functions/request_presence_validation.php
@@ -2,9 +2,8 @@
  function presenceValidation($request, $required_fields){
      $errors = array();
      $request_fields = array();
-     foreach($required_fields as $field){
-         echo 'checking presence of '. $field;
-         if(isset($request[$field]) && $request[$field]!=''){
+     foreach($required_fields as $field){         
+         if(!isset($request[$field]) || $request[$field] ==''){
              array_push($errors,array("field"=>$field, "message" => "is required"));
          } else {
              $request_fields[$field] = $request[$field];
