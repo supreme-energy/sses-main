d2272c8d4c6aa43494a4ed3ec525c01c1190e912
Tyler <tyler.hall@acimacredit.com>
2019-07-18 18:03:15 -0600

    added welllog endpoints

---
+++ web/api/welllog/update.php
@@ -0,0 +1,59 @@
+ <?php
+include ("../api_header.php");
+$id = $_REQUEST['id'];
+$field_names = array(
+    'fault',
+    'sectdip',
+    'secttot',
+    'sectbot',
+    'scalebias',
+    'scalefactor'
+);
+$updates_array = array();
+foreach ($field_names as $field_name) {
+    if (isset($_REQUEST[$field_name])) {
+        $value = $_REQUEST[$field_name];
+        $query_field_name = $field_name;
+        if ($field_name == 'unixtime_src') {
+            $query_field_name = 'srcts';
+        }
+        array_push($updates_array, "$field_name = '$value'");
+    }
+}
+if (count($updates_array) > 0) {
+    $query = "update wellogs set " . implode($updates_array, ',') . " where id=$id";
+    $db->DoQuery($query);
+}
+exec("./sses_gva -d $seldbname ");
+exec("./sses_cc -d $seldbname");
+exec("./sses_cc -d $seldbname -p");
+exec("./sses_af -d $seldbname");
+$db->DoQuery("SELECT * FROM welllogs where id='$id'");
+$db->FetchRow();
+$tablename = $db->FetchField('tablename');
+$results = array(
+    "id" => $id,
+    "tablename" => $tablename,
+    "realname" => $db->FetchField('realname'),
+    "startmd" => $db->FetchField('startmd'),
+    "endmd" => $db->FetchField('endmd'),
+    "starttvd" => $db->FetchField('starttvd'),
+    "endtvd" => $db->FetchField('endtvd'),
+    "startvs" => $db->FetchField('startvs'),
+    "endvs" => $db->FetchField('endvs'),
+    "startdepth" => $db->FetchField('startdepth'),
+    "enddepth" => $db->FetchField('enddepth'),
+    "fault" => $db->FetchField('fault'),
+    "sectdip" => $db->FetchField('dip'),
+    "secttot" => $db->FetchField('tot'),
+    "sectbot" => $db->FetchField('bot'),
+    "scalebias" => $db->FetchField('scalebias'),
+    "scalefactor" => $db->FetchField('scalefactor')
+);
+include ("../read_welllog_include.php");
+$results['data'] = $data;
+echo json_encode(array(
+    "status" => "Success",
+    "welllog" => $results
+));
+?>
