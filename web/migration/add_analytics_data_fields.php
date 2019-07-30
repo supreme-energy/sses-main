72b30ae4e1d9460707c53f06e5c19fbfca0d92e9
Tyler <tyler.hall@acimacredit.com>
2019-07-30 13:12:34 -0600

    added migration

---
+++ web/migration/add_analytics_data_fields.php
@@ -0,0 +1,28 @@
+<?php 
+$tables  = Array('wellinfo');
+$columns = Array(Array('analytics_address', 'text'), Array('analytics_job_id', 'integer'), Array('analytics_well_connected',  'boolean'));
+require_once("../dbio.class.php");
+$db=new dbio("sgta_index");
+$db->OpenDb();
+echo "\ndbupdate: Checking database for updates...\n";
+$query = "select * from dbindex";
+$db->DoQuery($query);
+$dbnames = Array('sgta_template');
+while($db->FetchRow()){
+    array_push($dbnames, $db->FetchField('dbname'));
+}
+$db->CloseDb();
+foreach($dbnames as $dbname){
+    $db=new dbio($dbname);
+    $db->OpenDb();
+    foreach($tables as $tt){
+        foreach($columns as $tc){
+            $col_name = $tc[0];
+            $col_type = $tc[1];
+            $query = "alter table $tt add $col_name $col_type";
+            echo $dbname.":".$query. "\n";
+            $db->DoQuery($query);
+        }
+    }
+}
+?>
