b2c1b8e38550894fe5a43498602301c05837a419
Tyler <tyler.hall@acimacredit.com>
2019-08-13 12:53:53 -0600

    fixed show

--- web/api/survey/history/show.php
+++ web/api/survey/history/show.php
@@ -1,18 +1,12 @@
 <?php 
 include ("../../api_header.php");
 $group_id = $_REQUEST['id'];
-$with_data = '1';
 $query="select * from deleted_survey_group order where id = $group_id";
 $db->DoQuery($query);
-$deleted_groups = array();
 while($row = $db->FetchRow()){
-    $this_group = array("id" => $group_id, "created" => $row['created']);
-    $group_id = $row['id'];   
-    if($with_data){
-        include ("./cleaned.php");
-        $this_group ['data'] = $data;
-    }
-    array_push($deleted_groups, $this_group);
+    $this_group = array("id" => $group_id, "created" => $row['created']);    
+    include ("./cleaned.php");
+    $this_group ['data'] = $data;
 }
-echo json_encode($deleted_groups[0]);
+echo json_encode($this_group);
 ?>
