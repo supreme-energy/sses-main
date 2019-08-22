1f0992c2e27e25d8784d05b51bdedfbf6e38b1db
Tyler <tyler.hall@acimacredit.com>
2019-08-22 08:39:38 -0600

    added calculation refresh on data deletion

--- web/api/projection/delete.php
+++ web/api/projection/delete.php
@@ -4,5 +4,9 @@
  $query = "delete from projections where id = ". $id;
  $db->DoQuery($query);
  $response = array("status"=>"success", "message"=>"projection deleted");
+ exec("../sses_gva -d $seldbname ");
+ exec("../sses_cc -d $seldbname");
+ exec("../sses_cc -d $seldbname -p");
+ exec ("../sses_af -d $seldbname");
  echo json_encode($response);
  ?>
