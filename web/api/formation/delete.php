96a29fd70b85bc624f8bf50443a688ac8b3afdc7
Tyler <tyler.hall@acimacredit.com>
2019-09-16 10:05:38 -0600

    removed echos

--- web/api/formation/delete.php
+++ web/api/formation/delete.php
@@ -2,10 +2,8 @@
 include ("../api_header.php");
 $id = $_REQUEST['id'];
 $query = "delete from addforms where id='$id'";
-echo $query;
 $db->DoQuery($query);
 $query = "delete from addformsdata where infoid= '$id'";
-echo $query;
 $db->DoQuery($query);
 echo json_encode(array("status"=>"success", "message"=>"formation deleted"));
 ?>
