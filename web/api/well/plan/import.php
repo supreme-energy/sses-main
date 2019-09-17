5a351c2471ec3b6ecf866301552df053c72c5a0f
Tyler <tyler.hall@acimacredit.com>
2019-09-17 10:15:25 -0600

    added wellplan upload

---
+++ web/api/well/plan/import.php
@@ -0,0 +1,55 @@
+<?php
+include("../../api_header.php");
+$debug=false;
+
+require 'HTTP/Upload.php';
+// print_r($HTTP_POST_FILES);
+$upload = new http_upload('en');
+$file = $upload->getFiles('userfile');
+if (PEAR::isError($file)) {
+	die ($file->getMessage());
+}
+if ($file->isValid()) {
+	// $file->setName('uniq');
+	$file->setName('wellplan.dat');
+	$dest_dir = '.../../../tmp/';
+	$dest_name = $file->moveTo($dest_dir);
+	if (PEAR::isError($dest_name)) {
+		die ($dest_name->getMessage());
+	}
+	$real = $file->getProp('real');
+	// echo "Uploaded $real as $dest_name in $dest_dir\n";
+} elseif ($file->isMissing()) {
+    echo json_encode(array('status'=>'error', 'message'=>'file missing'));
+    exit;
+} elseif ($file->isError()) {
+    echo json_encode(array('status'=>'error', 'message'=>$file->errorMsg()));	
+	exit;
+}
+
+$result=$db->DoQuery("DELETE FROM \"wellplan\";");
+if($result==FALSE) die("<pre>Error on SQL statement for table: wellplan\n</pre>");
+
+$filename="../../../tmp/wellplan.dat";
+$infile=fopen("$filename", "r");
+if(!$infile){
+    echo json_encode(array('status'=>'error', 'message'=>"File not found: $filename\n"));
+    exit;
+}
+
+$db->DoQuery("BEGIN TRANSACTION");
+while (($data = fgetcsv($infile, 5000, ",")) !== FALSE) {
+	$md=$data[0];
+	$inc=$data[1];
+	$azm=$data[2];
+	$result=$db->DoQuery("INSERT INTO wellplan (md,inc,azm) VALUES ('$md','$inc','$azm');");
+	if($result==FALSE) die("<pre>Error on SQL statement for table: wellplan\n</pre>");
+}
+$db->DoQuery("COMMIT");
+$db->CloseDb();
+fclose($infile);
+
+$output = shell_exec("../../../sses_cc -d $seldbname -w");
+// $output = shell_exec("./sses_cc -d $seldbname -w -i ./tmp/wellplan.dat");
+header("Location: ../../wellplan.php?seldbname=$seldbname");
+?>
