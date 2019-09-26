7729b71985331617b0fc89a15e6b06e17b049ace
Tyler <tyler.hall@acimacredit.com>
2019-09-26 10:53:23 -0600

    fixing issues with delete

--- web/api/job/delete.php
+++ web/api/job/delete.php
@@ -7,17 +7,21 @@ $db=new dbio("postgres");
 $db->OpenDb();
 $db->DoQuery('SELECT datname FROM pg_database WHERE datistemplate = false;');
 $db2 = new dbio("sgta_index");
+$db3 = new dbio("sgta_index");
 //$db2->DoQuery("delete from dbindex where name='$todelete'");
 echo "delete from dbindex where name='$todelete'";
 $db2->OpenDb();
+$db3->OpenDb();
 while($db->FetchRow()){
     $name = $db->FetchField("datname");
     if(in_array($name, $never_delete) === true){
         continue;
     }
     if(strpos($name, 'sgta_') !== false ){
-        $db2->DoQuery("select * from dbindex where name='$name'");
-        if(!$db2->FetchRow()){
+        $db3->DoQuery("select * from dbindex where name='$name'");
+        $res = $db3->FetchRow();
+        print_r($res);
+        if($res){
             //$db2->DoQuery("Drop database if exists $name");
             echo "Drop database if exists $name\n";
         }
