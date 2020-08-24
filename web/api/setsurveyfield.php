7d2558a21b5716859f8c2cfe38708836e168acfe
Tyler <tyler.hall@acimacredit.com>
2020-08-23 19:54:48 -0600

    fix for settin field bug

--- web/api/setsurveyfield.php
+++ web/api/setsurveyfield.php
@@ -3,6 +3,8 @@ include("api_header.php");
 $id = $_REQUEST['id'];
 $field_names = array('md', 'inc', 'azm', 'tvd', 'vs', 'ns', 'ew', 'ca', 'cd', 'dl', 'cl', 'bot', 'dip', 'fault', 'created_at', 'unixtime_src');
 $updates_array = array();
+$welllog_updates = array();
+$update_wellogifexsists = false;
 foreach($field_names as $field_name){
 	if(isset($_REQUEST[$field_name])){
 		$value = $_REQUEST[$field_name];
@@ -10,11 +12,25 @@ foreach($field_names as $field_name){
 		if($field_name == 'unixtime_src'){
 			$query_field_name = 'srcts';
 		}
-		array_push($updates_array, "$field_name = '$value'");	
+		array_push($updates_array, "$field_name = '$value'");
+		if($field_name == 'dip' || $field_name == 'fault'){
+		    $update_wellogifexsists = true;
+		    array_push($welllog_updates, "$field_name = '$value'");
+		}
 	}
 }
 if(count($updates_array) > 0 ){
 	$query = "update surveys set ". implode($updates_array, ',') . " where id=$id";
+	$db->DoQuery($query);
+    $query = "select md from surveys where id=$id";
+    $db->DoQuery($query);
+    $row = $db->FetchRow();
+    $lmd = $row['md'];
+	$query = "select id from welllogs where startmd > $lmd and endmd <= $lmd limit 1";
+	$db->DoQuery($query);
+	$row = $db->FetchRow();
+	$wellogid = $row['id'];
+	$query = "update welllogs set ".implode($welllog_updates, ',')." where id=$id";
 	$db->DoQuery($query);
 }
 exec("../sses_gva -d $seldbname ");
