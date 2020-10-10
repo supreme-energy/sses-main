dba67d918be0e09f28a86ef9040f1d6388aedd0a
Tyler <tyler.hall@acimacredit.com>
2020-10-10 13:47:10 -0600

    added missing requires

--- web/api/las/import_add_data.php
+++ web/api/las/import_add_data.php
@@ -1,7 +1,8 @@
 <?php 
-include ("../../api_header.php");
-require_once('../../../classes/LasFileConnection.class.php');
-
+include ("../api_header.php");
+require_once('../../classes/LasFileConnection.class.php');
+require_once("../../classes/WitsmlData.class.php");
+include("../../readwellinfo.inc.php");
 $obj = new LasFileConnection($_REQUEST);
 $obj->import_add_data();
 
