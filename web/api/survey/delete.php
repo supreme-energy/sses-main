94fdbac3c87927e487e192cad3538d1d03e6c25e
Tyler <tyler.hall@acimacredit.com>
2021-01-18 13:45:05 -0700

    fix to delete final

--- web/api/survey/delete.php
+++ web/api/survey/delete.php
@@ -1,5 +1,23 @@
  <?php 
  include("../api_header.php");
+ function delete_associated_data($db, $start_md, $cur_md) {
+     $query = "select * from welllogs where startmd > $start_md and endmd <= $cur_md";
+     $db->DoQuery($query);
+     $del_querys = "";
+     $drop_querys = "";
+     while($wlrow = $db->FetchRow()){
+         $welllogid = $wlrow['id'];
+         $drop_querys .= "drop table ". $wlrow['tablename'].";";
+         $del_querys .= "delete from welllogs where id = $welllogid;";
+     }
+     $db->DoQuery("select * from edatalogs;");
+     while($db->FetchRow()){
+         $tn = $db->FetchField('tablename');
+         $del_querys .="delete from \"$tn\" where md > $start_md;";
+     }
+     $db->DoQuery($del_querys);
+     $db->DoQuery($drop_querys);               
+ } 
  $id = $_REQUEST['id'];
  $query = "select * from surveys where plan=0 and id = " .$id;
  $db->DoQuery($query);
@@ -13,32 +31,19 @@
         $start_md = $prev_row['md'];
         $query = "delete from surveys where id = ". $id;
         $db->DoQuery($query);
-        $query = "select * from welllogs where startmd > $start_md and endmd <= $cur_md";
-        $db->DoQuery($query);
-        $del_querys = "";
-        $drop_querys = "";
-        while($wlrow = $db->FetchRow()){
-            $welllogid = $wlrow['id'];            
-            $drop_querys .= "drop table ". $wlrow['tablename'].";";
-            $del_querys .= "delete from welllogs where id = $welllogid;";
-        }
-        $db->DoQuery("select * from edatalogs;");    
-        while($db->FetchRow()){
-            $tn = $db->FetchField('tablename');
-            $del_querys .="delete from \"$tn\" where md > $start_md;";
-        }        
-        $db->DoQuery($del_querys);
-        $db->DoQuery($drop_querys);               
+        delete_associated_data($db, $start_md, $cur_md);
         $response = array("status"=>"success", "message"=>"survey deleted");
-        exec("../../sses_gva -d $seldbname ");
-        exec("../../sses_cc -d $seldbname");
-        exec("../../sses_cc -d $seldbname -p");
-        exec ("../../sses_af -d $seldbname");
     } else {
+        delete_associated_data($db, $cur_md, $cur_md*2);
         $response = array("status"=>"failed", "message"=>"cannot delete tie in row");
     }
+    exec("../../sses_gva -d $seldbname ");
+    exec("../../sses_cc -d $seldbname");
+    exec("../../sses_cc -d $seldbname -p");
+    exec ("../../sses_af -d $seldbname");
  } else {
     $response = array("status"=>"failed", "message"=>"no survey with id $id found");
  }
+
  echo json_encode($response);
  ?>
